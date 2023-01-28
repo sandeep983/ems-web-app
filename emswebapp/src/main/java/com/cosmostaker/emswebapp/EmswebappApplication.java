3d05d53cff6248b899bf08fac0ee395a06b023cf
sandeep983 <sandeeppatel983@hotmail.com>
2023-01-28 16:57:17 +0530

    initial commit

---
+++ emswebapp/src/main/java/com/cosmostaker/emswebapp/EmswebappApplication.java
@@ -0,0 +1,13 @@
+package com.cosmostaker.emswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmswebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmswebappApplication.class, args);
+	}
+
+}
