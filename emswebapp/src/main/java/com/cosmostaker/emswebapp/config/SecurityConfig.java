3369c7a191f8c67f6b3f5e36a19ade43d419602d
sandeep983 <sandeeppatel983@hotmail.com>
2023-02-01 19:22:10 +0530

    update

--- emswebapp/src/main/java/com/cosmostaker/emswebapp/config/SecurityConfig.java
+++ emswebapp/src/main/java/com/cosmostaker/emswebapp/config/SecurityConfig.java
@@ -67,105 +67,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
             .and()
             .logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login?logout")
 				.invalidateHttpSession(true)
-                .clearAuthentication(true)
+				.clearAuthentication(true)
 				.permitAll();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-// import org.springframework.security.web.SecurityFilterChain;
-
-
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
-
-//     @Bean
-//     public BCryptPasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-
-
-
-//     // Storing admin details in memory
-//     @Bean
-//     public InMemoryUserDetailsManager userDetailsManager() {
-
-//         UserDetails cosmostaker = User.builder()
-//             .username("Cosmostaker")
-//             .password(passwordEncoder().encode("cosmos@123"))
-//             .roles("EMPLOYEE")
-//             .build();
-
-//         UserDetails admin = User.builder()
-// 			.username("Admin")
-// 			.password(passwordEncoder().encode("admin@123"))
-// 			.roles("EMPLOYEE", "ADMIN")
-// 			.build();
-        
-//         return new InMemoryUserDetailsManager(cosmostaker, admin);        
-//     }
-
-    
-
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//     	return http
-// 		.authorizeRequests(configurer ->
-// 			configurer
-//                 //.antMatchers("/").permitAll()
-//                 .antMatchers("/register**").permitAll()
-//                 .antMatchers("/resources/**").permitAll()
-//                 .antMatchers("/updateEmployee*").hasRole("ADMIN")
-//                 .antMatchers("/deleteEmployee*").hasRole("ADMIN")
-//                 .antMatchers("/showAddEmployeeForm*").hasAnyRole("ADMIN", "EMPLOYEE")
-//                 .antMatchers("/saveEmployee*").hasAnyRole("ADMIN", "EMPLOYEE")
-//                 .anyRequest().authenticated())
-
-
-// 		.formLogin(configurer ->
-// 			configurer
-// 				.loginPage("/login")
-// 				.loginProcessingUrl("/authenticateUser")
-// 				.permitAll())
-		
-
-// 		.logout(configurer -> 
-// 			configurer
-//                 .invalidateHttpSession(true)
-//                 .clearAuthentication(true)
-// 				.permitAll())
-		
-
-// 		.exceptionHandling(configurer ->
-// 			configurer
-// 				.accessDeniedPage("/accessDenied"))
-				
-		
-// 		.build();
-//     }	
-// }
-
-
